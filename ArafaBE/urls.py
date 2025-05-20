82691e13af32b99896f98c23d7dd0e4fc9428693
abdulrhmanalqassas <abdulrhmanalqassas@gmail.com>
2025-05-20 21:09:53 +0300

    Add URL pattern for arafaAdmin admin site

--- ArafaBE/urls.py
+++ ArafaBE/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("crud/", include("crud.urls")),
     path("admin/", admin.site.urls),
+    path("arafaAdmin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
